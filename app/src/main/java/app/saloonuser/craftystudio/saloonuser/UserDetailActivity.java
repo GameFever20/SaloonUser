fad0a120ff1721ba5308c285dee6b151fbe08bb0
Aisha <priyank20patel@gmail.com>
2017-07-01 14:02:53 +0530

    Saloon detail activity completed and user detail

--- app/src/main/java/app/saloonuser/craftystudio/saloonuser/UserDetailActivity.java
+++ app/src/main/java/app/saloonuser/craftystudio/saloonuser/UserDetailActivity.java
@@ -1,12 +1,19 @@
 package app.saloonuser.craftystudio.saloonuser;
 
 import android.graphics.Color;
+import android.media.Image;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.github.paolorotolo.appintro.AppIntro;
@@ -19,20 +26,33 @@ public class UserDetailActivity extends AppCompatActivity {
 
     public static User USER;
 
-    EditText mUserNameEdittext , mUserAgeEdittext , mUserGenderEdittext ;
+    EditText mUserNameEdittext, mUserAgeEdittext, mUserGenderEdittext;
 
+    ImageView mMaleImage, mFemaleImage;
+
+    TextView mUserGenderTextview;
+
+    static String GENDER;
+
+    LinearLayout mUserDetailLinearlayout;
+    Button mUploadBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_detail);
 
-        mUserNameEdittext =(EditText)findViewById(R.id.userDetail_userName_edittext);
-        mUserGenderEdittext =(EditText)findViewById(R.id.userDetail_userGender_edittext);
-        mUserAgeEdittext =(EditText)findViewById(R.id.userDetail_userAge_edittext);
+        mUserNameEdittext = (EditText) findViewById(R.id.userDetail_userName_edittext);
+        mUserGenderEdittext = (EditText) findViewById(R.id.userDetail_userGender_edittext);
+        mUserAgeEdittext = (EditText) findViewById(R.id.userDetail_userAge_edittext);
 
+        mMaleImage = (ImageView) findViewById(R.id.user_detail_male_imageview);
+        mFemaleImage = (ImageView) findViewById(R.id.user_detail_female_imageview);
+        mUserDetailLinearlayout = (LinearLayout) findViewById(R.id.user_genderdetail_linearlayout);
 
+        mUserGenderTextview = (TextView) findViewById(R.id.userDetail_userGender_textview);
 
+        mUploadBtn = (Button) findViewById(R.id.user_detail_upload_button);
 
 
     }
@@ -52,9 +72,9 @@ public class UserDetailActivity extends AppCompatActivity {
             @Override
             public void onUserUpload(boolean isSuccessful) {
 
-                if (isSuccessful){
+                if (isSuccessful) {
                     Toast.makeText(UserDetailActivity.this, "User uploaded", Toast.LENGTH_SHORT).show();
-                }else{
+                } else {
                     Toast.makeText(UserDetailActivity.this, "Failed to upload User", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -65,9 +85,35 @@ public class UserDetailActivity extends AppCompatActivity {
     private void creteUser() {
         USER = new User();
         USER.setUserName(mUserNameEdittext.getText().toString().trim());
-        USER.setUserGender(mUserGenderEdittext.getText().toString().trim());
+        //  USER.setUserGender(mUserGenderEdittext.getText().toString().trim());
+        USER.setUserGender(GENDER);
         USER.setUserAge(Integer.valueOf(mUserAgeEdittext.getText().toString().trim()));
         USER.setUserUID(LoginActivity.USERUID);
 
+    }
+
+    public void femaleSelection(View view) {
+        mFemaleImage.setImageResource(R.drawable.femalecolor);
+        mMaleImage.setImageResource(R.drawable.malefinal);
+        GENDER = "Female";
+        Toast.makeText(this, "Hey! Beautiful", Toast.LENGTH_SHORT).show();
+    }
+
+    public void maleSelection(View view) {
+        mFemaleImage.setImageResource(R.drawable.female_copy);
+        mMaleImage.setImageResource(R.drawable.malecolor);
+        GENDER = "Male";
+        Toast.makeText(this, "Hey! HandSome", Toast.LENGTH_SHORT).show();
+
+    }
+
+
+    public void userDetailNext(View view) {
+        mUserDetailLinearlayout.setVisibility(View.GONE);
+        mUserNameEdittext.setVisibility(View.VISIBLE);
+        mUserAgeEdittext.setVisibility(View.VISIBLE);
+        mUploadBtn.setVisibility(View.VISIBLE);
+        mUserGenderTextview.setText(GENDER);
+        mUserGenderTextview.setVisibility(View.VISIBLE);
     }
 }
