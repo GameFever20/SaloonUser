57ea9cdcff91941559d68d1ec5ed8ab3c5595eb6
Aisha <priyank20patel@gmail.com>
2017-07-11 00:12:05 +0530

    order detail designing

--- app/src/main/java/utils/AnimationUtil.java
+++ app/src/main/java/utils/AnimationUtil.java
@@ -16,24 +16,25 @@ public class AnimationUtil {
     public static void animate(RecyclerView.ViewHolder holder, boolean goesDown) {
 
 
+        /*
         YoYo.with(Techniques.FadeInLeft)
                 .duration(1000)
                 .repeat(1)
                 .playOn(holder.itemView);
+*/
 
-        /*
         AnimatorSet animatorSet = new AnimatorSet();
 
-        ObjectAnimator animateScaleX = ObjectAnimator.ofFloat(holder.itemView, "scaleX", 0.5f, 0.8f, 1.0f);
+      //  ObjectAnimator animateScaleX = ObjectAnimator.ofFloat(holder.itemView, "scaleX", 0.5f, 0.8f, 1.0f);
 
         ObjectAnimator animateTranslateY = ObjectAnimator.ofFloat(holder.itemView, "translationY", goesDown == true ? 350 : -350, 0);
 
-        ObjectAnimator animateTranslateX = ObjectAnimator.ofFloat(holder.itemView, "translationX", 30, -30, 20, -20, 10, -10, 5, -5, 0);
+//        ObjectAnimator animateTranslateX = ObjectAnimator.ofFloat(holder.itemView, "translationX", 30, -30, 20, -20, 10, -10, 5, -5, 0);
 
-        animatorSet.playTogether(animateTranslateY, animateScaleX);
-        animatorSet.setDuration(500);
+        animatorSet.playTogether(animateTranslateY);
+        animatorSet.setDuration(1000);
         animatorSet.start();
-        */
+
 
     }
 }
